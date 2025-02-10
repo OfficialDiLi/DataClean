a166e85e10ef662a2de961225329b3f02342c7cd
MINKA'IL YAHAYA IBRAHIM <138785225+YIMMIY1389@users.noreply.github.com>
2025-02-10 08:01:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import streamlit as st
 import pandas as pd
 
-
 st.title("Simple Data Analysis App")
-
 
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
 if uploaded_file is not None:
-
     df = pd.read_csv(uploaded_file)
 
     st.subheader("Data Sample")
@@ -19,19 +16,19 @@ if uploaded_file is not None:
 
     st.subheader("Filter Data")
     columns = df.columns.tolist()
-    selected_column = st.selectbox("Select colunm to filter by", columns)
-    unique_value = df[selected_column].unique()
-    selected_value = st.selectbox("select value", unique_value)
+    selected_column = st.selectbox("Select column to filter by", columns)
+    unique_values = df[selected_column].unique()
+    selected_value = st.selectbox("Select value", unique_values)
 
-    filtered_df = df[df[selected_column] == unique_value]
+    # Corrected line: replaced 'unique_value' with 'selected_value'
+    filtered_df = df[df[selected_column] == selected_value]
     st.write(filtered_df)
 
     st.subheader("Plot Data")
-    x_column = st.selectbox("select the X-axis", columns)
-    y_column = st.selectbox("select the Y-axis", columns)
+    x_column = st.selectbox("Select the X-axis", columns)
+    y_column = st.selectbox("Select the Y-axis", columns)
 
     if st.button("Generate Plot"):
-        st.line_chart(filtered_df.set_index(x_column)[y_column])
+        st.line_chart(data=filtered_df, x=x_column, y=y_column)
 else:
     st.write("Waiting on file upload...")
-
